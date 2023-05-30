944f54d0005b07058feb5b09ce823e679512703b
Nikolai <kolamba.r@gmail.com>
2023-05-30 15:33:25 +0300

    HW 4

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -24,3 +24,14 @@ class TestNode(unittest.TestCase):
         queue.enqueue('data3')
 
         assert str(queue) == "data1\ndata2\ndata3"
+
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+
+        assert queue.head.data == 'data1'
+        assert queue.head.next_node.data == 'data2'
+        assert queue.tail.data == 'data3'
+        assert queue.tail.next_node is None
