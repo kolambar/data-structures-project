6855ab8c5910a36e0f146d42031017641e601ef3
Nikolai <kolamba.r@gmail.com>
2023-06-02 16:01:41 +0300

    HW 5

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+from src.linked_list import Node, LinkedList
+import unittest
+
+
+class TestNode(unittest.TestCase):
+
+    def test_insert_beginning(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        assert str(ll) == "{'id': 1} -> None"
+        ll.insert_beginning({'id': 2})
+        assert str(ll) == "{'id': 2} -> {'id': 1} -> None"
+
+
+    def test_insert_at_end(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        assert str(ll) == "{'id': 1} -> {'id': 2} -> None"
+
+    def test_Node(self):
+        test_block = Node({'id': 1})
+        assert str(test_block) == "data {'id': 1} next_node None"
